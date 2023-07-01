233445eb48ad3b6585628465f5ca3c5dc4c6166c
sankafodey <cctahlil@gmail.com>
2023-07-02 00:51:48 +0300

    five

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,19 +8,15 @@
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
-int print_sign(int n)
-{
-        if (n > 0)
-        {
-                _putchar('+');
-                return (1);
-        } else if (n == 0)
-				{
-                _putchar(48);
-                return (0);
-        } else if (n < 0)
-        {
-                _putchar('-');
-        }
-                return (-1);
+int print_sign(int n) {
+  if (n > 0) {
+    _putchar('+');
+    return (1);
+  } else if (n == 0) {
+    _putchar(48);
+    return (0);
+  } else if (n < 0) {
+    _putchar('-');
+  }
+  return (-1);
 }
